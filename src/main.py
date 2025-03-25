1f5376ecf4591388722e80f486b58192be950014
Anastasiya <naszh99@gmail.com>
2025-03-25 22:22:53 +0300

    add message_handler(Command(start))

--- src/main.py
+++ src/main.py
@@ -17,3 +17,23 @@ bot = Bot(token=config.bot_key, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=storage) # хранилище состояний в оперативной памяти
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s] %(message)s', level=logging.INFO, )
+
+@dp.message_handler(Command("start"), state=None) # задаем название команды start
+async def welcome(message):
+    joinedFile = open("user.txt", "r") # создаем файл, в который будем записывать id пользователя
+    joinedUsers = set()
+    for line in joinedFile: # цикл, в котором проверяем имеется ли такой id в файле user
+        joinedUsers.add(line.strip())
+
+    if not str(message.chat.id) in joinedUsers: # делаем запись в файл user нового id
+        joinedFile = open("user.txt", "a")
+        joinedFile.write(str(message.chat.id)+ "\n")
+        joinedUsers.add(message.chat.id)
+
+    await bot.send_message(message.chat.id, f"ПРИВЕТ, *{message.from_user.first_name},* БОТ РАБОТАЕТ",
+                           reply_markup=keyboard.start, parse_mode='Markdown')
+    # после проверки и записи выводим сообщение с именем пользователя и отображаем кнопки
+
+if __name__ == '__main__':
+    print('Бот запущен!') # чтобы бот работал всегда с выводом в начале вашего любого текста
+executor.start_polling(dp)
